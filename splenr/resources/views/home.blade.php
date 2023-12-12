d9d75b0339abbf3e48aa03e73a9ef66ee5596731
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-12-12 13:01:47 +0800

    added a images and structures for the home page

--- splenr/resources/views/home.blade.php
+++ splenr/resources/views/home.blade.php
@@ -48,19 +48,357 @@
         border: 2px solid var(--primary-color);
         color:var(--text-color-light)
     }
-  </style>
+
+    h2 span{
+        color:var(--primary-color)
+    }
+
+    .description-icon {
+        font-size: 60px;
+        color:var(--primary-color);
+        border: 2px solid var(--navbar-bg);
+        border-radius: 50%
+    }
+
+    .description-box {
+        border: 2px solid var(--text-color-light);
+    }
+
+    .description-box:hover {
+        cursor: pointer;
+        border: 2px solid var(--primary-color);
+        border-radius: 10px;
+        box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px;
+    }
+
+    .card-img-top {
+        height:200px
+    }
+
+    .popular-btn {
+        top: 2px;
+        right:2px;
+        
+    }
+
+    .company-img {
+        height: 100px
+    }
+
+    .card {
+        box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px;
+    }
+
+    .card:hover {
+        cursor: pointer;
+        box-shadow: rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px;
+    }
+
+</style>
+
 <div class="container-fluid px-4 py-5 d-flex align-items-center justify-content-center">
     <div class="row mt-5 align-items-center">
         <div class="col-lg-6 col-md-6 px-4 mx-auto mt-5 align-items-center d-flex justify-content-center">
             <div class="hero-text">
                 <h1 class="fw-bolder">Land the Perfect Electrician Position</h1>
                 <p class="fw-medium mt-2 fs-6">Assisting electricians to find their perfect fit in the industry.</p>
-                <a href="#"  class="btn browse-jobs-btn px-4 py-3 fw-semibold">BROWSE JOBS</a>
+                <a href="#"  class="btn browse-jobs-btn p-3 fw-semibold">BROWSE JOBS</a>
             </div>
         </div>
         <div class="col-lg-6 col-md-6 d-flex align-items-center justify-content-center">
             <img src="{{ asset('image/hero-image.png') }}" alt="Electrician Jobs" class="hero-image img-fluid mt-5">
         </div>
+    </div>
+</div>
+<div class="container-fluid px-4 pt-5 text-center d-flex align-items-center justify-content-center">
+    <div class="row d-flex align-items-center justify-content-center text-center">
+        <div class="description-box col-lg-3 col-md-3 col-sm-6 text-center d-flex mb-4  p-2">
+            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <i class="bi bi-briefcase description-icon px-4 py-2"></i>
+            </div>
+            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <div class="row m-0 p-0">
+                    <div class="col-12 m-0 p-0">
+                        <p class="fw-medium my-0 py-0 fs-5">Live Jobs</p>
+                    </div>
+                    <div class="col-12 my-0 p-0">
+                        <h2 class="fw-bolder m-0 p-0 text-warning">120+</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="description-box col-lg-3 col-md-3 col-sm-6 text-center d-flex mb-4  p-2">
+            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <i class="bi bi-buildings description-icon px-4 py-2"></i>
+            </div>
+            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <div class="row m-0 p-0">
+                    <div class="col-12 m-0 p-0">
+                        <p class="fw-medium m-0 p-0 fs-5">Companies</p>
+                    </div>
+                    <div class="col-12 m-0 p-0">
+                        <h2 class="fw-bolder m-0 p-0 text-warning">100+</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="description-box col-lg-3 col-md-3 col-sm-6 text-center d-flex mb-4 p-2">
+            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <i class="bi bi-person-circle description-icon px-4 py-2"></i>
+            </div>
+            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <div class="row m-0 p-0">
+                    <div class="col-12 m-0 p-0">
+                        <p class="fw-medium m-0 p-0 fs-5">Candidates</p>
+                    </div>
+                    <div class="col-12 m-0 p-0">
+                        <h2 class="fw-bolder m-0 p-0 text-warning">150+</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="description-box col-lg-3 col-md-3 col-sm-6 text-center d-flex mb-4 p-2">
+            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <i class="bi bi-file-earmark-plus-fill description-icon px-4 py-2"></i>
+            </div>
+            <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <div class="row m-0 p-0">
+                    <div class="col-12 m-0 p-0">
+                        <p class="fw-medium m-0 p-0 fs-5">New Jobs</p>
+                    </div>
+                    <div class="col-12 m-0 p-0">
+                        <h2 class="fw-bolder m-0 p-0 text-warning">50+</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid px-4 py-5">
+    <h2 class="fw-bolder text-center mb-5">OUR TRUSTED <span>COMPANY</span></h2>
+    <div class="row">
+        <div class="col-lg-2 col-md-2 col-sm-4 col-6 d-flex align-items-center justify-center">
+            <img src="{{ asset('image/WESTCO corporation.png') }}" alt="WESTCO" class="company-img img-fluid">
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-4 col-6 d-flex align-items-center justify-center">
+            <img src="{{ asset('image/Knauf_Logo.png') }}" alt="Knauf" class="company-img img-fluid">
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-4 col-6 d-flex align-items-center justify-center">
+            <img src="{{ asset('image/Meralco.png') }}" alt="MERALCO" class="company-img img-fluid">
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-4 col-6 d-flex align-items-center justify-center">
+            <img src="{{ asset('image/pilmico.jfif') }}" alt="pilmico" class="company-img img-fluid">
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-4 col-6 d-flex align-items-center justify-center">
+            <img src="{{ asset('image/PIV.jfif') }}" alt="Premium Infinite Ventures Inc." class="company-img img-fluid">
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-4 col-6 d-flex align-items-center justify-center">
+            <img src="{{ asset('image/TG.png') }}" alt="Trends Group In" class="company-img img-fluid">
+        </div>
+    </div>
+</div>
+<div class="container-fluid px-4 py-5">
+    <h2 class="fw-bolder mb-5 text-center">TRENDING JOB <span>POSITIONS</span></h2>
+    <div class="row px-2">
+        <div class="category col-lg-3 col-md-3 col-sm-6 col-12 mb-4">
+            <div class="d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title fw-semibold">Facility Electrician</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="category col-lg-3 col-md-3 col-sm-6 col-12 mb-4">
+            <div class="d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title fw-semibold">Master Electrician</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="category col-lg-3 col-md-3 col-sm-6 col-12 mb-4">
+            <div class="d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title fw-semibold">Maintenance Electrician</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="category col-lg-3 col-md-3 col-sm-6 col-12 mb-4">
+            <div class="d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title fw-semibold">Lead Electrical Engineer</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="category col-lg-3 col-md-3 col-sm-6 col-12 mb-4">
+            <div class="d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title fw-semibold">Junior Electrical Engineer</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="category col-lg-3 col-md-3 col-sm-6 col-12 mb-4">
+            <div class="d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title fw-semibold">Service Technician</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="category col-lg-3 col-md-3 col-sm-6 col-12 mb-4">
+            <div class="d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title fw-semibold">Electrical Technician</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="category col-lg-3 col-md-3 col-sm-6 col-12 mb-4">
+            <div class="d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title fw-semibold">Maintenance Staff</h5>
+                      <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid px-4 py-5">
+    <h2 class="fw-bolder text-center mb-5">POPULAR JOB <span>LOCATIONS</span></h2>
+    <div class="row px-2">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-12 job-location mb-4">
+            <div class="job-location-card d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('image/cebu-location.jpg') }}" class="card-img-top" alt="Cebu">
+                    <div class="card-body">
+                        <button class="btn btn-warning text-white position-absolute popular-btn">Popular</button>
+                        <p class="card-text fw-bold text-center">CEBU</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-12 job-location mb-4">
+            <div class="job-location-card d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('image/quezon-location.jpg') }}" class="card-img-top" alt="Quezon">
+                    <div class="card-body">
+                        <button class="btn btn-warning text-white position-absolute popular-btn">Popular</button>
+                        <p class="card-text fw-bold text-center">QUEZON</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-12 job-location mb-4">
+            <div class="job-location-card d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('image/cavite-location.webp') }}" class="card-img-top img-fluid" alt="Cavite">
+                    <div class="card-body">
+                        <button class="btn btn-warning text-white position-absolute popular-btn">Popular</button>
+                        <p class="card-text fw-bold text-center">CAVITE</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-12 job-location mb-4">
+            <div class="job-location-card d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('image/makati-location.webp') }}" class="card-img-top img-fluid" alt="Makati City">
+                    <div class="card-body">
+                        <button class="btn btn-warning text-white position-absolute popular-btn">Popular</button>
+                      <p class="card-text fw-bold text-center">MAKATI</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-12 job-location mb-4">
+            <div class="job-location-card d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('image/pasay-location.jpg') }}" class="card-img-top" alt="Pasay City">
+                    <div class="card-body">
+                        <button class="btn btn-warning text-white position-absolute popular-btn">Popular</button>
+                        <p class="card-text fw-bold text-center">PASAY</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-12 job-location mb-4">
+            <div class="job-location-card d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('image/taguig-location.jpg') }}" class="card-img-top" alt="Taguig City">
+                    <div class="card-body">
+                        <button class="btn btn-warning text-white position-absolute popular-btn">Popular</button>
+                        <p class="card-text fw-bold text-center">TAGUIG</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-12 job-location mb-4">
+            <div class="job-location-card d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('image/pasig-location.jpg') }}" class="card-img-top img-fluid" alt="Pasig City">
+                    <div class="card-body">
+                        <button class="btn btn-warning text-white position-absolute popular-btn">Popular</button>
+                        <p class="card-text fw-bold text-center">PASIG</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-12 job-location mb-4">
+            <div class="job-location-card d-flex align-items-center justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{ asset('image/paranaque-location.webp') }}" class="card-img-top img-fluid" alt="Parañaque City">
+                    <div class="card-body">
+                        <button class="btn btn-warning text-white position-absolute popular-btn">Popular</button>
+                      <p class="card-text fw-bold text-center">PARAÑAQUE</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </div>
 @endsection
--- splenr/resources/views/layouts/app.blade.php
+++ splenr/resources/views/layouts/app.blade.php
@@ -40,6 +40,7 @@
       .navbar {
         box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
         background: var(--navbar-bg);
+        z-index:1000
       }
 
       .login-btn, .logout-btn {
@@ -51,6 +52,10 @@
         background:var(--text-color-dark);
         border: 2px solid var(--primary-color);
         color:var(--text-color-light)!important
+      }
+
+      .navbar-toggler-icon {
+        border: none
       }
     </style>
 </head>
