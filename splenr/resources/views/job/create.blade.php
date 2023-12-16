226a9b8a057b67a3fd1583bfa3efe7c20e53c1dd
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-12-16 13:56:31 +0800

    added the form where the employer can post a job

--- splenr/resources/views/job/create.blade.php
+++ splenr/resources/views/job/create.blade.php
@@ -1,9 +1,57 @@
 @extends('layouts.admin.main')
 
 @section('content')
-<div class="container mt-5"></div>
+
+<div class="container mt-3 justify-content-center px-4">
     <div class="row justify-content-center">
-        form
+        <div class="col-md-8 my-5">
+            <h2 class="fw-bolder mb-5">Post A Job</h2>
+            <form action="#" method="POST">@csrf
+                <div class="form-group mb-4">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" id="title" class="form-control">
+                </div>
+                <div class="form-group mb-4">
+                    <label for="description">Description</label>
+                    <textarea type="text" name="description" id="description" class="form-control"></textarea>
+                </div>
+                <div class="form-group mb-4">
+                    <label for="description">Roles and Responsibility</label>
+                    <textarea type="text" name="roles" id="description" class="form-control"></textarea>
+                </div>
+                <div class="form-group mb-4">
+                    <label for="job_type" class="mb-2">Job Types</label>
+                    <div class="form-check mb-2">
+                        <input class="form-check-input" type="radio" name="job_type" id="Fulltime" value="Fulltime">
+                        <label for="Fulltime" class="form-check-label">Fulltime</label>
+                    </div>
+                    <div class="form-check mb-2">
+                        <input class="form-check-input" type="radio" name="job_type" id="Parttime" value="Fulltime">
+                        <label for="Parttime" class="form-check-label">Parttime</label>
+                    </div>
+                    <div class="form-check mb-2">
+                        <input class="form-check-input" type="radio" name="job_type" id="Casual" value="Casual">
+                        <label for="Casual" class="form-check-label">Casual</label>
+                    </div>
+                    <div class="form-check mb-2">
+                        <input class="form-check-input" type="radio" name="job_type" id="Contract" value="Contract">
+                        <label for="Contract" class="form-check-label">Contract</label>
+                    </div>
+    
+                </div>
+                <div class="form-group mb-4">
+                    <label for="address">Address</label>
+                    <input type="text" name="address" id="address" class="form-control">
+                </div>
+                <div class="form-group mb-4">
+                    <label for="title">Application Closing Date</label>
+                    <input type="date" name="date" id="date" class="form-control">
+                </div>
+
+                <button type="submit" class="btn btn-success">Post a Job</button>
+                
+            </form>
+        </div>
     </div>
 </div>
 
--- splenr/resources/views/layouts/admin/footer.blade.php
+++ splenr/resources/views/layouts/admin/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
         <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+            <div class="text-muted">Copyright &copy; splenr 2023</div>
             <div>
                 <a href="#">Privacy Policy</a>
                 &middot;
-                <a href="#">Terms &amp; Conditions</a>
+                <a href="#">Terms & Conditions</a>
             </div>
         </div>
     </div>
