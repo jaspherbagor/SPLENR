604518978c7dcfd884d37f8bcc19653a326affd0
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-12-17 14:47:30 +0800

    added comment to know the summernote cdn link and script

--- splenr/resources/views/layouts/admin/footer.blade.php
+++ splenr/resources/views/layouts/admin/footer.blade.php
@@ -21,6 +21,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+{{-- Summernote CND Script --}}
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 <script>
 $( function() {
--- splenr/resources/views/layouts/admin/header.blade.php
+++ splenr/resources/views/layouts/admin/header.blade.php
@@ -10,5 +10,6 @@
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
         <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+        {{-- Summernote CDN Link --}}
         <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
     </head>
