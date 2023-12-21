d1f54adbe433405d7b0cfc0e227f363f04c0addb
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-12-21 09:18:35 +0800

    added an error message to the employer update form

--- splenr/resources/views/profile/index.blade.php
+++ splenr/resources/views/profile/index.blade.php
@@ -7,6 +7,9 @@
         @if(Session::has('success'))
             <div class="alert alert-success">{{ Session::get('success') }}</div>
         @endif
+        @if(Session::has('error'))
+            <div class="alert alert-danger">{{ Session::get('error') }}</div>
+        @endif
 
         <h2 class="fw-bolder mb-4">Update Your Profile</h2>
 
