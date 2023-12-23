9c5141a2338d3bb4328077098223a5933d59e7db
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-12-23 15:31:57 +0800

    designed applicants display page

--- splenr/resources/views/applicants/show.blade.php
+++ splenr/resources/views/applicants/show.blade.php
@@ -2,17 +2,43 @@
 
 @section('content')
 
-<div class="container mt-3 justify-content-center px-4">
-    <div class="row justify-content-center">
+<div class="container mt-3 px-4">
+    <div class="row">
         <div class="col-md-10 my-4">
             <h2 class="fw-bolder">{{ $listing->title }}</h2>
         </div>
         @foreach($listing->users as $user)
-            Name: {{ $user->name }}
-            Email: {{ $user->email }}
-            <a href="#">Download Resume</a>
+            <div class="card mt-5">
+                <div class="row g-0">
+                    
+                    <div class="col-auto">
+                        @if($user->profile_pic)
+                        <img src="{{ Storage::url($user->profile_pic) }}" alt="Profile Picture" class="rounded-circle profile-image" >
+                        @else 
+                        <img src="https://placehold.co/400" alt="Profile Picture" class="rounded-circle profile-image" >
+
+                        @endif
+                    </div>
+                    <div class="col-auto">
+                        <div class="card-body">
+                            <p class=="fw-bold">{{ $user->name }}</p>
+                            <p class="card-text">{{ $user->email }}</p>
+                            <p class="card-text">{{ $user->created_at }}</p>
+                        </div>
+                    </div>
+                    <div class="col-auto ms-auto align-self-center">
+                        <a href="#" class="btn btn-warning">Download Resume</a>
+                    </div>
+                </div>
+            </div>
         @endforeach
     </div>
 </div>
+
+<style>
+    .profile-image {
+        width:150px
+    }
+</style>
 
 @endsection
