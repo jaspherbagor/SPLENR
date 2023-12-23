b09fdf9a68e352fd7ff80a8751f39697e3d2753f
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-12-23 15:50:48 +0800

    fixed the sidebar

--- splenr/resources/views/layouts/admin/sidebar.blade.php
+++ splenr/resources/views/layouts/admin/sidebar.blade.php
@@ -13,16 +13,16 @@
                     Jobs
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-                <a class="nav-link" href="{{ route('applicants.index') }}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                    Applicants
-                </a>
                 <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
                         <a class="nav-link" href="{{ route('job.create') }}">Create Job</a>
                         <a class="nav-link" href="{{ route('job.index') }}">Your Jobs</a>
                     </nav>
                 </div>
+                <a class="nav-link" href="{{ route('applicants.index') }}">
+                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                    Applicants
+                </a>
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                     <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
                     Pages
