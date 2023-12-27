4b42f9fba4f3fabb0ecfa6f8343c86b4a425b97d
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-12-27 21:22:29 +0800

    added an initial code to the db query

--- splenr/app/Http/Controllers/JoblistingController.php
+++ splenr/app/Http/Controllers/JoblistingController.php
@@ -9,7 +9,16 @@ class JoblistingController extends Controller
 {
     public function index(Request $request)
     {
-        $jobs =  Listing::with('profile')->get();
+        $salary = $request->query('sort');
+        $date = $request->query('date');
+        $jobType = $request->query('job_type');
+
+        $listing = Listing::query();
+        if($salary === 'salary_high_to_low') {
+            Listing::orderBy('salary', 'descending')
+        }
+
+        //$jobs =  Listing::with('profile')->get();
         return view('jobs', compact('jobs'));
     }
 
