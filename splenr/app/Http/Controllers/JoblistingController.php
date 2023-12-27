24788e07cc514918d56e124e16b40a1e4484e028
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-12-28 07:07:33 +0800

    added another function in which the jobs are sorted/arranged from oldest to newest or vice versa

--- splenr/app/Http/Controllers/JoblistingController.php
+++ splenr/app/Http/Controllers/JoblistingController.php
@@ -15,10 +15,17 @@ class JoblistingController extends Controller
         $jobType = $request->query('job_type');
 
         $listings = Listing::query();
+
         if($salary === 'salary_high_to_low') {
             $listings->orderBy('salary', 'desc');
         } else if($salary === 'salary_low_to_high') {
             $listings->orderBy('salary', 'asc');
+        }
+
+        if($date === 'latest') {
+            $listings->orderBy('created_at', 'desc');
+        } else if($date === 'oldest') {
+            $listings->orderBy('created_at', 'asc');
         }
 
         
