e43aca75e03392955261f5bc61e041fcae63c2e7
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-12-29 09:06:30 +0800

    updated the primary card in the dashboard

--- splenr/resources/views/dashboard.blade.php
+++ splenr/resources/views/dashboard.blade.php
@@ -32,9 +32,10 @@
             <div class="row">
                 <div class="col-xl-3 col-md-6">
                     <div class="card bg-primary text-white mb-4">
-                        <div class="card-body">Primary Card</div>
+                        <div class="card-body">Total Jobs: (<span class="fw-bold">{{ \App\Models\Listing::where('user_id', auth()->id())->count() }}</span>)</div>
                         <div class="card-footer d-flex align-items-center justify-content-between">
-                            <a class="small text-white stretched-link" href="#">View Details</a>
+
+                            <a class="small text-white stretched-link" href="{{ route('job.index') }}">View Details</a>
                             <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                         </div>
                     </div>
