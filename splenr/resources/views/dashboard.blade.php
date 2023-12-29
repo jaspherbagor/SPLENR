57530a73d980e4ff0124a965f89084623288d519
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-12-29 09:15:31 +0800

    commented out the danger card

--- splenr/resources/views/dashboard.blade.php
+++ splenr/resources/views/dashboard.blade.php
@@ -58,7 +58,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-3 col-md-6">
+                {{-- <div class="col-xl-3 col-md-6">
                     <div class="card bg-danger text-white mb-4">
                         <div class="card-body">Danger Card</div>
                         <div class="card-footer d-flex align-items-center justify-content-between">
@@ -66,7 +66,7 @@
                             <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
             <div class="row">
                 <div class="col-xl-6">
