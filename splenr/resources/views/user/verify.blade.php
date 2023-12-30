4bdf48dff2de0add51504d1cf7902459a8e6e906
Jaspher Bagor <jaspherbagor0357@gmail.com>
2023-12-30 20:55:29 +0800

    fixed the alert message of the verify account interface

--- splenr/resources/views/user/verify.blade.php
+++ splenr/resources/views/user/verify.blade.php
@@ -3,6 +3,10 @@
 @section('content')
 <div class="container px-4 py-5 mt-5">
     <div class="d-flex align-items-center justify-content-center mt-5">
+        <div class="container col-md-9">
+        @if(Session::has('success'))
+        <div class="alert alert-success mb-2">{{ Session::get('success') }}</div>
+        @endif
         <div class="card p-0">
             <div class="card-header fw-semibold">Verify Account</div>
             <div class="card-body">
@@ -10,6 +14,7 @@
                     <a href="{{ route('resend.email') }}">Resend verification email.</a>
                 </p>
             </div>
+        </div>
         </div>
     </div>
 </div>
