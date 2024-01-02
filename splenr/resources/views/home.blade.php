2df7305603be8151d81356e930943109850de4de
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-02 20:50:34 +0800

    added a border to the cards inn the home page

--- splenr/resources/views/home.blade.php
+++ splenr/resources/views/home.blade.php
@@ -87,6 +87,7 @@
 
     .card {
         box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px;
+        border: 2px solid var(--text-color-dark)
     }
 
     .card:hover {
