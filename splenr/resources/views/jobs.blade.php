b82cf6718ad73a9487c11378be61857ba80d11ce
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-04 21:50:30 +0800

    modified the styles of the listing cards in the joblisting page

--- splenr/resources/views/jobs.blade.php
+++ splenr/resources/views/jobs.blade.php
@@ -6,57 +6,57 @@
     <div class="mt-5">
         <h2 class="fw-bolder text-center my-4">JOB LISTINGS</h2>
         
-        <div class="row px-3">
-            <div class="col-lg-2 col-md-2 col-sm-4 filter-column p-0">
-                <div class="filter-container container-fluid p-3">
+        <div class="row px-3 mt-4">
+            <div class="col-lg-3 col-md-3 col-sm-3 filter-column mb-4">
+                <div class="filter-container container-fluid p-2">
                     <h4 class="fw-bold mb-4">FILTER JOBS</h4>
 
                     <h5 class="fw-semibold mt-3 mb-2">SALARY</h5>
                     <hr>
-                    <li class="ms-2"><a href="{{ route('listing.index', ['sort' => 'salary_high_to_low']) }}" class="text-decoration-none text-black filter-item fw-medium">HIGH TO LOW</a></li>
+                    <li class="ms-2"><a href="{{ route('listing.index', ['sort' => 'salary_high_to_low']) }}" class="text-decoration-none text-black filter-item fw-medium">High to Low</a></li>
                     <hr>
-                    <li class="ms-2"><a href="{{ route('listing.index', ['sort' => 'salary_low_to_high']) }}" class="text-decoration-none text-black filter-item fw-medium">LOW TO HIGH</a></li>
+                    <li class="ms-2"><a href="{{ route('listing.index', ['sort' => 'salary_low_to_high']) }}" class="text-decoration-none text-black filter-item fw-medium">Low to High</a></li>
                     <hr>
 
                     <h5 class="fw-semibold mt-3 mb-2">DATE</h5>
                     <hr>
-                    <li class="ms-2"><a href="{{ route('listing.index', ['date' => 'latest']) }}" class="text-decoration-none text-black filter-item fw-medium">LATEST</a></li>
+                    <li class="ms-2"><a href="{{ route('listing.index', ['date' => 'latest']) }}" class="text-decoration-none text-black filter-item fw-medium">Latest</a></li>
                     <hr>
-                    <li class="ms-2"><a href="{{ route('listing.index', ['date' => 'oldest']) }}" class="text-decoration-none text-black filter-item fw-medium">OLDEST</a></li>
+                    <li class="ms-2"><a href="{{ route('listing.index', ['date' => 'oldest']) }}" class="text-decoration-none text-black filter-item fw-medium">Oldest</a></li>
                     <hr>
 
                     <h5 class="fw-semibold mt-3 mb-2">JOB TYPE</h5>
                     <hr>
-                    <li class="ms-2"><a href="{{ route('listing.index', ['job_type' => 'Fulltime']) }}" class="text-decoration-none text-black filter-item fw-medium">FULLTIME</a></li>
+                    <li class="ms-2"><a href="{{ route('listing.index', ['job_type' => 'Fulltime']) }}" class="text-decoration-none text-black filter-item fw-medium">Fulltime</a></li>
                     <hr>
-                    <li class="ms-2"><a href="{{ route('listing.index', ['job_type' => 'Parttime']) }}" class="text-decoration-none text-black filter-item fw-medium">PARTTIME</a></li>
+                    <li class="ms-2"><a href="{{ route('listing.index', ['job_type' => 'Parttime']) }}" class="text-decoration-none text-black filter-item fw-medium">Parttime</a></li>
                     <hr>
-                    <li class="ms-2"><a href="{{ route('listing.index', ['job_type' => 'Casual']) }}" class="text-decoration-none text-black filter-item fw-medium">CASUAL</a></li>
+                    <li class="ms-2"><a href="{{ route('listing.index', ['job_type' => 'Casual']) }}" class="text-decoration-none text-black filter-item fw-medium">Casual</a></li>
                     <hr>
-                    <li class="ms-2"><a href="{{ route('listing.index', ['job_type' => 'Contract']) }}" class="text-decoration-none text-black filter-item fw-medium">CONTRACT</a></li>   
+                    <li class="ms-2"><a href="{{ route('listing.index', ['job_type' => 'Contract']) }}" class="text-decoration-none text-black filter-item fw-medium">Contract</a></li>   
                 </div>           
             </div>
-            <div class="col-lg-10 col-md-10 col-sm-8 job-listing-column p-0">
-                <div class="row ms-1">
+            <div class="col-lg-9 col-md-9 col-sm-9 job-listing-column">
+                <div class="row align-items-center">
                     @foreach($jobs as $job)
                     <div class="col-lg-4 col-md-4">
                         <div class="card p-2 {{ $job->job_type }} mb-4">
                             <div class="text-end">
-                                <small class="badge text-bg-info">{{ $job->job_type }}</small>
+                                <small class="badge text-bg-dark">{{ $job->job_type }}</small>
                             </div>
                             <div class="text-center mt-2 p-3">
-                                <img src="{{ Storage::url($job->profile->profile_pic) }}" alt="logo" width="100" class="listing-company-logo rounded-circle">
+                                <img src="{{ Storage::url($job->profile->profile_pic) }}" alt="logo" class="listing-company-logo rounded-circle">
                                 <br>
-                                <span class="d-block fw-bold">{{ $job->title }}</span>
+                                <p class="d-block fw-bold listing-title">{{ $job->title }}</p>
                                 <hr>
-                                <span>{{ $job->profile->name }}</span>
+                                <p class="listing-company-name">{{ $job->profile->name }}</p>
                                 <div class="d-flex flex-row align-items-center justify-content-center">
-                                    <small class="ms-1">{{ $job->address }}</small>
+                                    <small class="ms-1 listing-address">{{ $job->address }}</small>
                                 </div>
                                 <div class="d-flex justify-content-between mt-3">
-                                    <span>₱{{ number_format($job->salary,2) }}</span>
+                                    <p class="listing-salary">₱{{ number_format($job->salary,2) }}</p>
                                     <a href="{{ route('job.show', [$job->slug]) }}">
-                                        <button class="btn btn-dark">Apply</button>
+                                        <button class="btn listing-apply-btn">Apply</button>
                                     </a>
                                 </div>
                             </div>
@@ -120,6 +120,28 @@
         font-weight:500 !important
     }
 
+    .listing-company-logo {
+        width: 60px;
+        height: 60px
+    }
+
+    .listing-title, .listing-company-name, .listing-address, .listing-salary
+    {
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
+
+    .listing-apply-btn {
+        background: var(--primary-color);
+        border: 2px solid var(--text-color-dark)
+    }
+
+    .listing-apply-btn:hover {
+        background: var(--text-color-dark);
+        border: 2px solid var(--primary-color);
+        color:var(--text-color-light)
+    }
     .Fulltime {
         background: green;
         color:#fff
