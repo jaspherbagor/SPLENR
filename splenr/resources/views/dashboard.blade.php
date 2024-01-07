c924626b5838a0ce07d9f650b59963e9eb747707
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-07 14:31:07 +0800

    modified the case of the job applicant word in the dashboard

--- splenr/resources/views/dashboard.blade.php
+++ splenr/resources/views/dashboard.blade.php
@@ -88,7 +88,7 @@
             <div class="card mb-4">
                 <div class="card-header">
                     <i class="fas fa-table me-1"></i>
-                    JOB APPLICANTS
+                    Job Applicants
                 </div>
                 <div class="card-body">
                     <table id="datatablesSimple">
