011138d00a56671f9eaa0a9957b602d53a31114d
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-08 22:07:10 +0800

    removed the unecessary spacing

--- splenr/app/Http/Controllers/UserController.php
+++ splenr/app/Http/Controllers/UserController.php
@@ -99,7 +99,7 @@ class UserController extends Controller
             return view('seeker.profile');
         } else {
             return back();
-        }   
+        }
     }
 
     public function changePassword(Request $request)
