05c82ca9424070f199b083ad09a17357a8bae18a
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-09 22:56:39 +0800

    updated the structure of the dashboard to align with the coding standard

--- splenr/resources/views/dashboard.blade.php
+++ splenr/resources/views/dashboard.blade.php
@@ -19,23 +19,37 @@
                 Hello, <span class="fw-semibold">{{ auth()->user()->name }}</span>!
                 @if(!auth()->user()->billing_ends)
                     @if(Auth::check() && auth()->user()->user_type == 'employer')
-                    <span>Your trial {{ now()->format('Y-m-d') > auth()->user()->user_trial ? 'was expired': 'will expire'  }} on {{ auth()->user()->user_trial}}</span>
+                    <span>Your trial
+                        {{ now()->format('Y-m-d') > auth()->user()->user_trial ? ' was expired': 'will expire'  }}
+                         on {{ auth()->user()->user_trial}}
+                    </span>
                     @endif
                 @endif
             </p>
             @if(Auth::check() && auth()->user()->user_type == 'employer')
-                <p>Your membership {{ now()->format('Y-m-d') > auth()->user()->billing_ends ? 'was expired': 'will expire'  }} on <span class="fw-semibold">{{ auth()->user()->billing_ends}}</span></p>
+                <p>
+                    Your membership
+                    {{ now()->format('Y-m-d') > auth()->user()->billing_ends ?
+                    ' was expired': ' will expire'  }} on
+                    <span class="fw-semibold">
+                        {{ auth()->user()->billing_ends}}
+                    </span>
+                </p>
             @endif
             <div class="row">
                 <div class="col-xl-4 col-md-6">
                     <div class="card bg-danger text-white mb-4">
                         <div class="card-body">
-                            <h1 class="fw-bolder fs-1">{{ \App\Models\Listing::where('user_id', auth()->id())->count() }}</h1>
+                            <h1 class="fw-bolder fs-1">
+                                {{ \App\Models\Listing::where('user_id', auth()->id())->count() }}
+                            </h1>
                             <div class="icon text-end"><i class="bi bi-suitcase-lg fs-2"></i></div>
                             <h6 class="fw-medium">Total Jobs</h6>
                         </div>
                         <div class="card-footer d-flex align-items-center justify-content-between">
-                            <a class="small text-white stretched-link fw-semibold" href="{{ route('job.index') }}">VIEW</a>
+                            <a class="small text-white stretched-link fw-semibold" href="{{ route('job.index') }}">
+                                VIEW
+                            </a>
                             <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                         </div>
                     </div>
@@ -48,7 +62,9 @@
                             <h6 class="fw-medium">Account</h6>
                         </div>
                         <div class="card-footer d-flex align-items-center justify-content-between">
-                            <a class="small text-white stretched-link fw-semibold" href="{{ route('user.profile') }}">VIEW</a>
+                            <a class="small text-white stretched-link fw-semibold" href="{{ route('user.profile') }}">
+                                VIEW
+                            </a>
                             <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                         </div>
                     </div>
@@ -56,12 +72,16 @@
                 <div class="col-xl-4 col-md-6">
                     <div class="card bg-success text-white mb-4">
                         <div class="card-body">
-                            <h1 class="fw-bolder text-uppercase fs-1">{{ App\Models\User::where('id', auth()->id())->first()->plan }}</h1>
+                            <h1 class="fw-bolder text-uppercase fs-1">
+                                {{ App\Models\User::where('id', auth()->id())->first()->plan }}
+                            </h1>
                             <div class="icon text-end"><i class="bi bi-unlock fs-2"></i></div>
                             <h6>Plan</h6>
                         </div>
                         <div class="card-footer d-flex align-items-center justify-content-between">
-                            <a class="small text-white stretched-link fw-semibold" href="{{ route('subscribe') }}">View</a>
+                            <a class="small text-white stretched-link fw-semibold" href="{{ route('subscribe') }}">
+                                View
+                            </a>
                             <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                         </div>
                     </div>
@@ -94,6 +114,9 @@
                 </div>
                 <div class="card-body">
                     <table id="datatablesSimple">
+                        <caption>
+                            This table contains the list of the job applicnts that the employer have posted.
+                        </caption>
                         <thead>
                             <tr>
                                 <th>Title</th>
@@ -110,8 +133,14 @@
                                 <td>{{ $listing->title }}</td>
                                 <td>{{ $listing->created_at->format('Y-m-d') }}</td>
                                 <td>{{ $listing->users_count }}</td>
-                                <td><a href="{{ route('job.show', [$listing->slug]) }}" class="text-secondary">View</a></td>
-                                <td><a href="{{ route('applicants.show', $listing->slug) }}" class="text-success">View</a></td>
+                                <td>
+                                    <a href="{{ route('job.show', [$listing->slug]) }}" class="text-secondary">View</a>
+                                </td>
+                                <td>
+                                    <a href="{{ route('applicants.show', $listing->slug) }}" class="text-success">
+                                        View
+                                    </a>
+                                </td>
                             </tr>
                             
                             @endforeach
