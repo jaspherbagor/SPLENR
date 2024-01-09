3e2044e6a807c031efe23b0bca4f865f9c6f7500
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-09 10:07:06 +0800

    updated the case of the class

--- splenr/app/Http/Middleware/doNotAllowUserToMakePayment.php
+++ splenr/app/Http/Middleware/doNotAllowUserToMakePayment.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class doNotAllowUserToMakePayment
+class DoNotAllowUserToMakePayment
 {
     /**
      * Handle an incoming request.
