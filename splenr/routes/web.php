8453f5d42da3cbb5d8e86f7a4f359b563f4e281a
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-09 10:32:50 +0800

    removed the commented out codes

--- splenr/routes/web.php
+++ splenr/routes/web.php
@@ -15,17 +15,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', [JoblistingController::class, 'homepage'])->name('homepage');
 
 Route::get('/contact', [ContactController::class, 'contact'])->name('contact');
