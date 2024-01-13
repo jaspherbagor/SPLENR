7f9b3ede6981c33939918b98fab6fe7309af340f
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-13 13:22:18 +0800

    properly aligned the elements in the navbar codes

--- splenr/resources/views/layouts/app.blade.php
+++ splenr/resources/views/layouts/app.blade.php
@@ -14,108 +14,103 @@
   <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg px-3 position-fixed container-fluid" data-bs-theme="light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">
-            <img src="{{ asset('image/SPLENR-LOGO.svg') }}" class="logo" alt="SPLENR Logo">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-           data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item me-4">
-                <a class="nav-link active fw-semibold text-black fs-6" aria-current="page" href="/">
-                  <i class="bi bi-house-door fs-5"></i> HOME</a>
-              </li>
+  <nav class="navbar navbar-expand-lg px-3 position-fixed container-fluid" data-bs-theme="light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          <img src="{{ asset('image/SPLENR-LOGO.svg') }}" class="logo" alt="SPLENR Logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item me-4">
+              <a class="nav-link active fw-semibold text-black fs-6" aria-current="page" href="/">
+                <i class="bi bi-house-door fs-5"></i> HOME</a>
+            </li>
 
-              @if(Auth::check())
-              <li class="nav-item me-4 dropdown text-decoration-none">
-                <a class="btn dropdown-toggle p-0 text-decoration-none" href="#"
-                 role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <img src="{{ Storage::url(auth()->user()->profile_pic) }}"
-                   width="40" height="40" class="rounded-circle" alt="profile_pic">
-                </a>
-              
-                <ul class="dropdown-menu text-decoration-none  p-0">
-                  @if(auth()->user()->user_type === 'seeker')
-                  <li class="nav-item my-0 py-0 ps-2">
-                    <a class="nav-link active fw-semibold text-black fs-6"
-                     aria-current="page" href="{{ route('seeker.profile') }}">
-                      <i class="bi bi-person fs-5"></i> PROFILE
-                    </a>
-                  </li>
-                  <li class="nav-item my-0 py-0 ps-2">
-                    <a class="nav-link fw-semibold text-black fs-6" href="{{ route('job.applied') }}">
-                      <i class="bi bi-card-list fs-5"></i>
-                       JOB APPLIED
-                    </a>
-                  </li>
-                  <li class="nav-item my-0 py-0 ps-2">
-                    <a class="nav-link fw-semibold text-danger fs-6"  id="logoutbtn" href="#">
-                      <i class="bi bi-box-arrow-left fs-5"></i> LOGOUT
-                    </a>
-                  </li>
-                  @else
-                  <li class="nav-item my-0 py-0 ps-2">
-                    <a class="nav-link fw-semibold text-black fs-6" href="{{ route('dashboard') }}">
-                      <i class="bi bi-speedometer fs-5"></i> DASHBOARD
-                    </a>
-                  </li>
-                  @endif
-                </ul>
-              </li>
-            @endif
-              
-              <li class="nav-item me-4">
-                <a class="nav-link fw-semibold text-black fs-6" href="{{ route('listing.index') }}">
-                  <i class="bi bi-briefcase fs-5"></i> JOBS
-                </a>
-              </li>
-              <li class="nav-item me-4">
-                <a class="nav-link fw-semibold text-black fs-6" href="/FAQs">
-                  <i class="bi bi-patch-question fs-5"></i> FAQs
-                </a>
-              </li>
-              <li class="nav-item me-4">
-                <a class="nav-link fw-semibold text-black fs-6" href="{{ route('contact') }}">
-                  <i class="bi bi-telephone fs-5"></i> CONTACT
-                </a>
-              </li>
-              @if(!Auth::check())
-              <li class="nav-item me-4">
-                <a class="nav-link fw-semibold text-black fs-6" href="{{ route('create.seeker') }}">
-                  <i class="bi bi-person fs-5"></i> JOB SEEKER
-                </a>
-              </li>
-              <li class="nav-item me-4">
-                <a class="nav-link fw-semibold text-black fs-6" href="{{ route('create.employer') }}">
-                  <i class="bi bi-building fs-5"></i> EMPLOYER
-                </a>
-              </li>
-              @endif
-            </ul>
-
-            @if(!Auth::check())
-            <div class="me-3">
-              <a href="{{ route('login') }}">
-                <button class="btn fw-semibold text-black login-btn fs-6">LOGIN</button>
+            @if(Auth::check())
+            <li class="nav-item me-4 dropdown text-decoration-none">
+              <a class="btn dropdown-toggle p-0 text-decoration-none" href="#"
+                role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="{{ Storage::url(auth()->user()->profile_pic) }}"
+                  width="40" height="40" class="rounded-circle" alt="profile_pic">
               </a>
-            </div>
+            
+              <ul class="dropdown-menu text-decoration-none  p-0">
+                @if(auth()->user()->user_type === 'seeker')
+                <li class="nav-item my-0 py-0 ps-2">
+                  <a class="nav-link active fw-semibold text-black fs-6"
+                    aria-current="page" href="{{ route('seeker.profile') }}">
+                    <i class="bi bi-person fs-5"></i> PROFILE
+                  </a>
+                </li>
+                <li class="nav-item my-0 py-0 ps-2">
+                  <a class="nav-link fw-semibold text-black fs-6" href="{{ route('job.applied') }}">
+                    <i class="bi bi-card-list fs-5"></i>
+                      JOB APPLIED
+                  </a>
+                </li>
+                <li class="nav-item my-0 py-0 ps-2">
+                  <a class="nav-link fw-semibold text-danger fs-6"  id="logoutbtn" href="#">
+                    <i class="bi bi-box-arrow-left fs-5"></i> LOGOUT
+                  </a>
+                </li>
+                @else
+                <li class="nav-item my-0 py-0 ps-2">
+                  <a class="nav-link fw-semibold text-black fs-6" href="{{ route('dashboard') }}">
+                    <i class="bi bi-speedometer fs-5"></i> DASHBOARD
+                  </a>
+                </li>
+                @endif
+              </ul>
+            </li>
+          @endif
+            
+            <li class="nav-item me-4">
+              <a class="nav-link fw-semibold text-black fs-6" href="{{ route('listing.index') }}">
+                <i class="bi bi-briefcase fs-5"></i> JOBS
+              </a>
+            </li>
+            <li class="nav-item me-4">
+              <a class="nav-link fw-semibold text-black fs-6" href="/FAQs">
+                <i class="bi bi-patch-question fs-5"></i> FAQs
+              </a>
+            </li>
+            <li class="nav-item me-4">
+              <a class="nav-link fw-semibold text-black fs-6" href="{{ route('contact') }}">
+                <i class="bi bi-telephone fs-5"></i> CONTACT
+              </a>
+            </li>
+            @if(!Auth::check())
+            <li class="nav-item me-4">
+              <a class="nav-link fw-semibold text-black fs-6" href="{{ route('create.seeker') }}">
+                <i class="bi bi-person fs-5"></i> JOB SEEKER
+              </a>
+            </li>
+            <li class="nav-item me-4">
+              <a class="nav-link fw-semibold text-black fs-6" href="{{ route('create.employer') }}">
+                <i class="bi bi-building fs-5"></i> EMPLOYER
+              </a>
+            </li>
             @endif
-            <form id="logout-form" action="{{ route('logout') }}" method="post" >@csrf</form>
+          </ul>
+
+          @if(!Auth::check())
+          <div class="me-3">
+            <a href="{{ route('login') }}">
+              <button class="btn fw-semibold text-black login-btn fs-6">LOGIN</button>
+            </a>
           </div>
+          @endif
+          <form id="logout-form" action="{{ route('logout') }}" method="post" >@csrf</form>
         </div>
-    </nav>
+      </div>
+  </nav>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-      crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>
-
-    @yield('content')
+  @yield('content')
     
 </body>
 </html>
