282012374d1f8fb981e864ad9bd65d4067834aee
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-13 13:23:30 +0800

    moved the scripts in the footer instead in the navbar

--- splenr/resources/views/layouts/footer.blade.php
+++ splenr/resources/views/layouts/footer.blade.php
@@ -95,3 +95,7 @@
         form.submit();
     })
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+crossorigin="anonymous"></script>
+<script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>
