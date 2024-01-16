1808cdcd25820464b8c2ea07de8b846d474d64e7
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-16 09:30:54 +0800

    fixed the date issue

--- splenr/app/Post/JobPost.php
+++ splenr/app/Post/JobPost.php
@@ -7,12 +7,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
 
- class JobPost {
+class JobPost
+{
 
     protected $listing;
     public function __construct(Listing $listing)
     {
-        $this->listing =$listing;
+        $this->listing = $listing;
     }
 
     public function getImagePath(Request $data)
@@ -20,7 +21,7 @@ use Carbon\Carbon;
         return $data->file('feature_image')->store('images', 'public');
     }
 
-    public function store(Request $data):void
+    public function store(Request $data): void
     {
         $imagePath = $this->getImagePath($data);
         $this->listing->feature_image = $imagePath;
@@ -31,19 +32,28 @@ use Carbon\Carbon;
         $this->listing->requirements = $data['requirements'];
         $this->listing->job_type = $data['job_type'];
         $this->listing->address = $data['address'];
-        $this->listing->application_close_date = Carbon::createFromFormat('d/m/Y',
-        $data['date'])->format('Y-m-d');
+
+        // Attempt to create a Carbon instance from the provided date string
+        try {
+            $parsedDate = Carbon::createFromFormat('m/d/Y', $data['date']);
+            $formattedDate = $parsedDate->format('Y-m-d');
+            $this->listing->application_close_date = $formattedDate;
+        } catch (\Exception $e) {
+            dd($e->getMessage()); // Debugging: Dump the exception message
+        }
+
         $this->listing->salary = $data['salary'];
-        $this->listing->slug = Str::slug($data['title']).'.'. Str::uuid();
+        $this->listing->slug = Str::slug($data['title']) . '.' . Str::uuid();
         $this->listing->save();
     }
 
-    public function updatePost(int $id, Request $data):void
+
+    public function updatePost(int $id, Request $data): void
     {
-        if($data->hasFile('feature_image')) {
+        if ($data->hasFile('feature_image')) {
             $this->listing->find($id)->update(['feature_image' => $this->getImagePath($data)]);
         }
 
         $this->listing->find($id)->update($data->except('feature_image'));
     }
- }
+}
