812c2dda05ee4918ee794648cae96daa964c80c1
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-16 09:48:13 +0800

    added an external css link for the admin navigation bar

--- splenr/resources/views/layouts/admin/header.blade.php
+++ splenr/resources/views/layouts/admin/header.blade.php
@@ -10,6 +10,7 @@
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
         <link rel="stylesheet" href="{{ asset('css/subscription.css') }}"/>
+        <link rel="stylesheet" href="{{ asset('css/adminnavbar.css') }}">
         <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
         rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
