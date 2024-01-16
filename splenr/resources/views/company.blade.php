f48685c190ef51b79790aa535bb77e94595f65a2
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-16 10:36:29 +0800

    modified the company job postings and added an external css

--- splenr/resources/views/company.blade.php
+++ splenr/resources/views/company.blade.php
@@ -5,7 +5,7 @@
 <div class="container px-4 py-5">
   <div class="row justify-content-center mt-5">
     <div class="col">
-      <div class="hero-section">
+      <div class="company-hero-section w-100">
       </div>
     </div>
   </div>
@@ -24,33 +24,39 @@
     {!! $company->about !!}
   </div>
 
-  <h3 class="fw-bolder mt-4 text-center">JOB POSTINGS</h3>
+  <h3 class="fw-bolder mt-4 text-center">JOB POSTS</h3>
   
-  <div class="row mt-3 align-items-center justify-content-center">
+  <div class="row mt-3 align-items-center justify-content-center px-2">
     @foreach($company->jobs as $job)
     <div class="col-md-6">
       <div class="card mb-3">
           <div class="card-body">
-              <h5 class="card-title">{{ $job->title }}</h5>
-              <p class="card-text">Location: {{ $job->address }}</p>
-              <p class="card-text">
+              <h5 class="card-title fw-bold">{{ $job->title }}</h5>
+              <p class="card-text py-0 my-0">Location: <span class="fw-bold">{{ $job->address }}</span></p>
+              <p class="card-text py-0 my-0">
                 Salary:
                 <span class="fw-bold">
                   ₱{{ number_format($job->salary, 2) }} per month
                 </span>
               </p>
-              <a href="{{ route('job.show', [$job->slug]) }}" class="btn btn-dark">View</a>
+              <p class="card-text py-0 my-0">
+                Application Deadline:
+                <span class="fw-bold">
+                  {{ \Carbon\Carbon::parse($job->application_close_date)->format('F j, Y') }}
+                </span>
+              </p>
+              <a href="{{ route('job.show', [$job->slug]) }}" class="btn btn-success btn-outline-dark fw-semibold">
+                VIEW
+              </a>
           </div>
       </div>
     </div>
     @endforeach
   </div>
 </div>
-<style>
+{{-- <style>
   .hero-section {
-    background-color:#f5f5f5;
-    width:100%;
-    height:200px;
+
   }
-</style>
+</style> --}}
 @endsection
--- splenr/resources/views/layouts/app.blade.php
+++ splenr/resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="{{ asset('css/navigation.css') }}">
   <link rel="stylesheet" href="{{ asset('css/jobs.css') }}">
   <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/company.css') }}">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg px-3 position-fixed container-fluid" data-bs-theme="light">
