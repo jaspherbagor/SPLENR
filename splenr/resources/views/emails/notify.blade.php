eec33c4281a304ae378fae29c14e97fec2d9d13d
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-17 09:28:29 +0800

    updated the email blades

--- splenr/resources/views/emails/notify.blade.php
+++ splenr/resources/views/emails/notify.blade.php
@@ -1,14 +1,9 @@
 <x-mail::message>
-# Introduction
 
 Congratulations! You are now a premium user.
 <p>Your purchase details:</p>
 <p>Plan: <span style="font-weight:bold">{{ $plan }}</span></p>
 <p>Your plan ends on: <span style="font-weight:bold">{{ $billingEnds }}</span></p>
-
-<x-mail::button :url="''">
-Post a Job
-</x-mail::button>
 
 Thanks,<br>
 splenr
