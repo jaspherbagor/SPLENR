8cb3ed7466e82eeadc53abecf92650cdca166567
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-20 20:36:00 +0800

    added a temporary view height of the about blade

--- splenr/resources/views/about.blade.php
+++ splenr/resources/views/about.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="about-section mt-5 px-4 py-5">
+<section class="about-section mt-5 px-4 py-5 vh-100">
     <h2 class="fw-bolder">This page is ongoing...</h2>
 </section>
 
