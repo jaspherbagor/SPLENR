a8144f82f647eb3817efd56e9b14850d47399972
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-20 20:40:31 +0800

    added a structure for the privacy policy page

---
+++ splenr/resources/views/privacy-policy.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="privacy-policy mt-5 px-4 py-5 vh-100">
+    <h2 class="fw-bolder">This page for the privacy policy is ongoing...</h2>
+</section>
+
+@include('layouts.footer')
+@endsection
