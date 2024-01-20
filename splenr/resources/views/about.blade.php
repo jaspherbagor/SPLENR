e10d207a6893fea885a278167d74e7e12053b477
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-20 20:28:48 +0800

    set up the structure of the about blade

---
+++ splenr/resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="about-section mt-5 px-4 py-5">
+    <h2 class="fw-bolder">This page is ongoing...</h2>
+</section>
+
+@include('layouts.footer')
+@endsection
