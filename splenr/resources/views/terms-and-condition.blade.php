f469029efbadcea241646be0bd18df9380b5f6eb
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-20 20:58:27 +0800

    added a structure of the terms and conditions page

---
+++ splenr/resources/views/terms-and-condition.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="terms-and-conditions mt-5 px-4 py-5 vh-100">
+    <h2 class="fw-bolder">This page for the terms and conditions is ongoing...</h2>
+</section>
+@include('layouts.footer')
+
+@endsection
