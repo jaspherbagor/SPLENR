ca6a12938e4f8c5937509cd835d87f33d48a181c
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-21 19:17:01 +0800

    updated the heading of the about blade

--- splenr/resources/views/about.blade.php
+++ splenr/resources/views/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section class="about-section mt-5 px-4 py-5 vh-100">
-    <h2 class="fw-bolder">This page is ongoing...</h2>
+    <h2 class="fw-bolder">ABOUT</h2>
 </section>
 
 @include('layouts.footer')
