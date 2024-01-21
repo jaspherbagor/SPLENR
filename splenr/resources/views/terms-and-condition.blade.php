a28d768d07a382935376adcaea9492b76dd07e91
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-21 19:20:29 +0800

    updated the heading of the terms and conditions

--- splenr/resources/views/terms-and-condition.blade.php
+++ splenr/resources/views/terms-and-condition.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section class="terms-and-conditions mt-5 px-4 py-5 vh-100">
-    <h2 class="fw-bolder">This page for the terms and conditions is ongoing...</h2>
+    <h2 class="fw-bolder">TERMS AND CONDITION</h2>
 </section>
 @include('layouts.footer')
 
