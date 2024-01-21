b22d728d97bb451a5d3c0218919bef603791c137
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-21 19:17:51 +0800

    centered the heading

--- splenr/resources/views/about.blade.php
+++ splenr/resources/views/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section class="about-section mt-5 px-4 py-5 vh-100">
-    <h2 class="fw-bolder">ABOUT</h2>
+    <h2 class="fw-bolder text-center">ABOUT</h2>
 </section>
 
 @include('layouts.footer')
