17653a4944940adfd423e440e5d128b770d7cd77
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-21 19:24:18 +0800

    centered the heading

--- splenr/resources/views/privacy-policy.blade.php
+++ splenr/resources/views/privacy-policy.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section class="privacy-policy mt-5 px-4 py-5 vh-100">
-    <h2 class="fw-bolder">PRIVACY POLICY</h2>
+    <h2 class="fw-bolder text-center">PRIVACY POLICY</h2>
 </section>
 
 @include('layouts.footer')
