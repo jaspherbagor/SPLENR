26bab74b0f1c3fa8acb3aeba2d6631832e386f21
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-21 19:21:16 +0800

    align the heading to the center

--- splenr/resources/views/terms-and-condition.blade.php
+++ splenr/resources/views/terms-and-condition.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section class="terms-and-conditions mt-5 px-4 py-5 vh-100">
-    <h2 class="fw-bolder">TERMS AND CONDITION</h2>
+    <h2 class="fw-bolder text-center">TERMS AND CONDITION</h2>
 </section>
 @include('layouts.footer')
 
