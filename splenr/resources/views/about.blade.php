ed6f3ad9bc4b1c13b4045789f4ccf623bd7e8464
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-21 19:19:10 +0800

    added a top margin of the heading

--- splenr/resources/views/about.blade.php
+++ splenr/resources/views/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section class="about-section mt-5 px-4 py-5 vh-100">
-    <h2 class="fw-bolder text-center">ABOUT</h2>
+    <h2 class="fw-bolder text-center mt-4">ABOUT</h2>
 </section>
 
 @include('layouts.footer')
