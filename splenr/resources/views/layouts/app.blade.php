f4e6eaf1384d6f526e4b07310eb82b3461da38e2
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-23 10:26:14 +0800

    moved the bootstrap link to the header to make the menu button functional even without the footer

--- splenr/resources/views/layouts/app.blade.php
+++ splenr/resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
   rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
   crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <link href="https://unpkg.com/filepond@^4/dist/filepond.css" rel="stylesheet" />
   <link rel="stylesheet" href="{{ asset('css/navigation.css') }}">
   <link rel="stylesheet" href="{{ asset('css/jobs.css') }}">
