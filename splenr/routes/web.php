e74d98a808660f42b74f15d9adf1e2ed859070de
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-01-24 12:24:13 +0800

    removed the constant login route

--- splenr/routes/web.php
+++ splenr/routes/web.php
@@ -51,13 +51,11 @@ Route::get('/jobs/{listing:slug}', [JoblistingController::class, 'show'])->name(
 // File upload route
 Route::post('/resume/upload', [FileUploadController::class, 'store'])->middleware('auth');
 
-// Define a constant for the login route
-define('LOGIN_ROUTE', '/login');
 
 // Email verification route
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
-    return redirect(LOGIN_ROUTE);
+    return redirect('/login');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 // User registration routes
@@ -71,8 +69,8 @@ Route::get('/register/employer', [UserController::class, 'createEmployer'])
     ->middleware(CheckAuth::class);
     
 Route::post('/register/employer', [UserController::class, 'storeEmployer'])->name('store.employer');
-Route::get(LOGIN_ROUTE, [UserController::class, 'login'])->name('login')->middleware(CheckAuth::class);
-Route::post(LOGIN_ROUTE, [UserController::class, 'postLogin'])->name('login.post');
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware(CheckAuth::class);
+Route::post('/login', [UserController::class, 'postLogin'])->name('login.post');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
 // User profile routes
